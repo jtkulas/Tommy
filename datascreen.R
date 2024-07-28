113ee5f29623e66f90da9ad6dbcac8e71ed01cf4
John-o Kulas <jtkulas@ergreports.com>
2024-07-28 14:58:57 -0500

    Add files via upload

---
+++ datascreen.R
@@ -0,0 +1,7 @@
+# library(googlesheets4)    ## 7/28/24 ! Client error: (400) FAILED_PRECONDITION
+
+data <- read_sheet("https://docs.google.com/spreadsheets/d/16_EuETQy8mUMPMmo1_NJuRoBo23qQQpj/edit?gid=276584609#gid=276584609")
+
+# map life expectancy
+
+# community census survey
