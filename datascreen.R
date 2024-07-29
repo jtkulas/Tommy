b78e682268bc84e8bf276c5bcc864520c3410ff2
John Kulas <jtkulas@ergreports.com>
2024-07-29 09:07:45 -0500

    desktop to laptop

--- datascreen.R
+++ datascreen.R
@@ -1,6 +1,6 @@
 # library(googlesheets4)    ## 7/28/24 ! Client error: (400) FAILED_PRECONDITION
 
-data <- read_sheet("https://docs.google.com/spreadsheets/d/16_EuETQy8mUMPMmo1_NJuRoBo23qQQpj/edit?gid=276584609#gid=276584609")
+data <- read.csv("Copy of UniversalScoreCardALL.xlsx - UniversalScoreCardX.csv")
 
 # map life expectancy
 
